13176fa1cf4d58adf77a2f3ff6b76acba36635c2
Daisuke Murase <typester@cpan.org>
2013-01-22 12:09:06 +0900

    exec-path まわり修正

--- conf/exec-path.el
+++ conf/exec-path.el
@@ -1,3 +1,8 @@
-(add-to-list 'exec-path
-             "~/homebrew/bin"
-             "~/perl5/perlbrew/perls/perl-5.14.1/bin")
+(setq shell-file-name "/bin/sh")
+
+(let*
+    ((path (list "~/homebrew/bin"
+                 "~/perl5/perlbrew/perls/perl-5.14.1/bin")))
+  (dolist (p path)
+    (add-to-list 'exec-path (expand-file-name p))
+    (setenv "PATH" (concat (expand-file-name p) ":" (getenv "PATH")))))

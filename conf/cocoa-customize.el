345fa03a240ace890c4a289c1d1432da6dca6f0d
Daisuke Murase <typester@cpan.org>
2013-01-23 10:31:27 +0900

    sync customize

--- conf/cocoa-customize.el
+++ conf/cocoa-customize.el
@@ -9,5 +9,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "#373b41" :foreground "#d2d2d2" :box nil))))
- '(mode-line-highlight ((t (:box nil)))))
+ '(cperl-array-face ((t (:foreground "yellow" :weight normal))))
+ '(cperl-hash-face ((t (:foreground "Red" :slant normal :weight normal))))
+ '(mode-line ((t (:background "#aed700" :foreground "black" :box nil))))
+ '(mode-line-highlight ((t (:box nil))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "#202020" :foreground "#d2d2d2" :box nil :weight light)))))

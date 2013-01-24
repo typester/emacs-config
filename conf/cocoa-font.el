fa0c68441d0c5a048085e8e7b820411a69b677c8
Daisuke Murase <typester@cpan.org>
2013-01-24 16:43:02 +0900

    unicode 文字が表示できなかった

--- conf/cocoa-font.el
+++ conf/cocoa-font.el
@@ -1,6 +1,9 @@
 (set-face-attribute 'default nil
                     :family "Menlo" :height 130)
 
+(set-fontset-font nil 'unicode
+                  (font-spec :family "Menlo"))
+
 (set-fontset-font nil 'japanese-jisx0208
                   (font-spec :family "Hiragino Kaku Gothic ProN"))
 

f8fb9d8883ea6c92a310a0aaaf32788f26b96229
Daisuke Murase <typester@cpan.org>
2013-01-30 16:03:56 +0900

    半角カナがでなかったので katakana-jisx0201 のフォント指定

--- conf/cocoa-font.el
+++ conf/cocoa-font.el
@@ -6,6 +6,8 @@
 
 (set-fontset-font nil 'japanese-jisx0208
                   (font-spec :family "Hiragino Kaku Gothic ProN"))
+(set-fontset-font nil 'katakana-jisx0201
+                  (font-spec :family "Hiragino Kaku Gothic ProN"))
 
 (setq face-font-rescale-alist
       '((".*Menlo.*" . 1.0)

5b58cfda121500b3a20fc319fd287aa000eeb6a7
Daisuke Murase <typester@cpan.org>
2013-02-01 11:19:31 +0900

    % で対応括弧に移動

--- conf/init.el
+++ conf/init.el
@@ -37,6 +37,15 @@
 ;; 対応する括弧を光らせる
 (show-paren-mode 1)
 
+;; % で対応する括弧に移動
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+	((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+	(t (self-insert-command (or arg 1)))))
+(define-key global-map (kbd "%") 'match-paren)
+
 ;; 色つける
 (require 'font-lock)
 (global-font-lock-mode t)

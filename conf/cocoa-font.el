5d3e99e20228afba48f23a174e65bdf419002f97
Daisuke Murase <typester@cpan.org>
2013-02-01 11:19:01 +0900

    ループで回すように

--- conf/cocoa-font.el
+++ conf/cocoa-font.el
@@ -4,10 +4,9 @@
 (set-fontset-font nil 'unicode
                   (font-spec :family "Menlo"))
 
-(set-fontset-font nil 'japanese-jisx0208
-                  (font-spec :family "Hiragino Kaku Gothic ProN"))
-(set-fontset-font nil 'katakana-jisx0201
-                  (font-spec :family "Hiragino Kaku Gothic ProN"))
+(dolist (code '(japanese-jisx0208 katakana-jisx0201))
+  (set-fontset-font nil code
+                    (font-spec :family "Hiragino Kaku Gothic ProN")))
 
 (setq face-font-rescale-alist
       '((".*Menlo.*" . 1.0)

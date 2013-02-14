65b41de50dd4448b45d42930c02741e70103fbe2
Daisuke Murase <typester@cpan.org>
2013-02-14 12:44:25 +0900

    開きなおしたあと元の場所に移動

--- conf/misc.el
+++ conf/misc.el
@@ -1,9 +1,10 @@
 ;; 開いてるバッファを開き直す Emacs 23.4 の cc-mode でなぜか indent が効かなくなったときに使う、、
 (defun reopen-buffer ()
   (interactive)
-  (let ((file-name (buffer-file-name)))
+  (let ((file-name (buffer-file-name))
+        (pos (point)))
     (progn
       (or file-name (error "this buffer is memory only buffer!"))
       (kill-buffer)
-      (find-file file-name))))
-
+      (find-file file-name)
+      (goto-char pos))))

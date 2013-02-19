ec513e873458f6becf45f6a42160d66cb4dca614
Daisuke Murase <typester@cpan.org>
2013-02-19 14:01:18 +0900

    ハングル表示

--- conf/cocoa-font.el
+++ conf/cocoa-font.el
@@ -4,6 +4,9 @@
 (set-fontset-font nil 'unicode
                   (font-spec :family "Menlo"))
 
+(set-fontset-font nil 'korean-ksc5601
+                  (font-spec :family "AppleGothic"))
+
 (dolist (code '(japanese-jisx0208 katakana-jisx0201))
   (set-fontset-font nil code
                     (font-spec :family "Hiragino Kaku Gothic ProN")))

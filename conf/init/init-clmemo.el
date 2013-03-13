5c0224598a0b5af855da29e595e6401331221bd3
Daisuke Murase <typester@cpan.org>
2013-03-13 14:33:48 +0900

    複数clmemoの設定

--- conf/init/init-clmemo.el
+++ conf/init/init-clmemo.el
@@ -7,4 +7,12 @@
 (defadvice clmemo-get-title (after clmemo-get-title-with-time () activate)
   (setq ad-return-value (concat (format-time-string "%H:%M ") ad-return-value)))
 
+(defun clmemo-with-file (memofile)
+  (interactive "Fclmemo file: ")
+  (let ((clmemo-file-name memofile))
+    (call-interactively 'clmemo)))
+
 (define-key global-map (kbd "C-x M") 'clmemo)
+(define-key global-map (kbd "C-x C-M") (lambda ()
+                                         (interactive)
+                                         (clmemo-with-file "~/clmemo.txt")))

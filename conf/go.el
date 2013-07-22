f85a98326932dafa5c0ee799a48d452d7b6f5c5c
Daisuke Murase <typester@cpan.org>
2013-07-22 14:05:00 +0900

    add key-binds

--- conf/go.el
+++ conf/go.el
@@ -10,3 +10,13 @@
 ;; go-eldoc
 (require 'go-eldoc)
 (add-hook 'go-mode-hook 'go-eldoc-setup)
+
+;; key-binds
+(add-hook 'go-mode-hook
+          '(lambda()
+             (setq c-basic-offset 4)
+             (setq indent-tabs-mode t)
+             (local-set-key (kbd "M-.") 'godef-jump)
+             (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+             (local-set-key (kbd "C-c a") 'go-import-add)
+             (local-set-key (kbd "C-c d") 'godoc)))

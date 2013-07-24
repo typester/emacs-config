31407563067eed0ea68af06b54aeea1bcf3eb1ab
Daisuke Murase <typester@cpan.org>
2013-07-24 13:33:15 +0900

    moe-themeおためし

--- conf/cocoa-customize.el
+++ conf/cocoa-customize.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-agenda-files (quote ("/Users/typester/Dropbox/org/personal/baby.org" "/Users/typester/Dropbox/org/personal/idea.org" "/Users/typester/Dropbox/org/personal/startup.org" "/Users/typester/Dropbox/org/projects/feed.org" "/Users/typester/Dropbox/org/projects/lobi.org" "/Users/typester/Dropbox/org/projects/monsterhunter.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

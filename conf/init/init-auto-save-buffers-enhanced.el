9af44cb0562133f2b3214313ab30c2e44310682d
Daisuke Murase <typester@cpan.org>
2013-11-07 07:11:12 +0900

    auto-saveするバッファはauto-revertも有効にする

--- conf/init/init-auto-save-buffers-enhanced.el
+++ conf/init/init-auto-save-buffers-enhanced.el
@@ -1,2 +1,12 @@
 (auto-save-buffers-enhanced-include-only-checkout-path t)
-(setq auto-save-buffers-enhanced-quiet-save-p t)
+
+;;;; これをonにするとauto-revertが効かないので一旦はずす
+;;(setq auto-save-buffers-enhanced-quiet-save-p t)
+
+;; auto-saveしてるバッファではauto-revertも有効にする
+(defadvice auto-save-buffers-enhanced-saver-buffer (after auto-save-buffers-enhanced-saver-buffer-with-auto-revert)
+  (auto-revert-mode t))
+(ad-activate-regexp "auto-save-buffers-enhanced-saver-buffer-with-auto-revert")
+
+;; auto-revertの間隔
+(setq auto-revert-interval 1)

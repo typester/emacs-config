00dd0ddd2fc2edd94ea048b380759c1b66939b6b
Daisuke Murase <typester@cpan.org>
2017-06-23 05:02:19 +0900

    typescript-modeのインデントが気にいらないので、.tsもweb-modeを使う

--- conf/init/init-tide.el
+++ conf/init/init-tide.el
@@ -14,11 +14,12 @@
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
-
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
 (add-hook 'web-mode-hook
           (lambda ()
-            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+            (when (or (string-equal "tsx" (file-name-extension buffer-file-name))
+                      (string-equal "ts" (file-name-extension buffer-file-name)))
               (tide-setup)
               (flycheck-mode +1)
               (setq flycheck-check-syntax-automatically '(save mode-enabled))

8cd2d34f856cd1e563676f193f118066b3926434
Daisuke Murase <typester@cpan.org>
2017-06-28 17:32:03 +0900

    adjust font size

--- conf/x-font.el
+++ conf/x-font.el
@@ -1,9 +1,9 @@
 ;; HiDPIだと文字大きすぎるので調整
 (if (>= (x-display-pixel-width) 3840)
     (set-face-attribute 'default nil
-                        :family "Ricty" :height 110)
+                        :family "Ricty" :height 105)
   (set-face-attribute 'default nil
-                      :family "Ricty" :height 130))
+                      :family "Ricty" :height 120))
 
 ;; ↑だけだと日本語が違うfontになっちゃうことがある
 (dolist (code '(japanese-jisx0208 katakana-jisx0201))

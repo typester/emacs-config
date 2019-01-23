39c4ff5e8c0409ea51386de24abdba46b00cfe45
Daisuke Murase <typester@cpan.org>
2019-01-23 14:28:35 +0900

    remove hidpi check temporary because x-display-pixel-width is currently void

--- conf/x-font.el
+++ conf/x-font.el
@@ -1,9 +1,5 @@
-;; HiDPIだと文字大きすぎるので調整
-(if (>= (x-display-pixel-width) 3840)
-    (set-face-attribute 'default nil
-                        :family "Ricty" :height 105)
-  (set-face-attribute 'default nil
-                      :family "Ricty" :height 120))
+(set-face-attribute 'default nil
+                    :family "Ricty" :height 130)
 
 ;; ↑だけだと日本語が違うfontになっちゃうことがある
 (dolist (code '(japanese-jisx0208 katakana-jisx0201))

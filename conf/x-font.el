153da6deb7667a4c9172cf044ca8d81884d22255
Daisuke Murase <typester@cpan.org>
2019-01-24 03:07:21 +0900

    try Source Han Code JP

--- conf/x-font.el
+++ conf/x-font.el
@@ -1,7 +1,7 @@
 (set-face-attribute 'default nil
-                    :family "Ricty" :height 130)
+                    :family "Source Han Code JP" :height 110)
 
 ;; ↑だけだと日本語が違うfontになっちゃうことがある
 (dolist (code '(japanese-jisx0208 katakana-jisx0201))
   (set-fontset-font nil code
-                    (font-spec :family "Ricty")))
+                    (font-spec :family "Source Han Code JP")))

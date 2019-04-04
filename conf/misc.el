7ce54558005ea79e6c35118187c33795f996a818
Daisuke Murase <typester@cpan.org>
2019-04-04 09:01:34 +0900

    add plantuml-mode

--- conf/misc.el
+++ conf/misc.el
@@ -2,3 +2,9 @@
 (el-get-bundle lua-mode)
 (el-get-bundle emacs-fish)
 (el-get-bundle dockerfile-mode)
+(el-get-bundle plantuml-mode :type github :pkgname "skuro/plantuml-mode"
+  (and (file-exists-p "/etc/arch-release")
+       (progn
+         (setq plantuml-jar-path "/usr/share/java/plantuml/plantuml.jar")
+         (setq plantuml-java-command "/usr/lib/jvm/java-11-openjdk/bin/java"))))
+

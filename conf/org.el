1783fda2fabb0408f8f7ff49ad7a74c0e72fec30
Daisuke Murase <typester@cpan.org>
2019-04-09 08:48:09 +0900

    change org-capture path too

--- conf/org.el
+++ conf/org.el
@@ -11,14 +11,14 @@
         '(
           ("a" "Archive" plain (file (lambda ()
                                     (let* ((slug (read-string "slug: "))
-                                           (dir "~/drive/org/public/unknownplace.org/archive"))
+                                           (dir "~/dev/src/github.com/typester/blog-entries/archive"))
                                       (require 'org-id)
                                       (make-directory dir t)
                                       (concat dir "/" slug ".org"))))
            "#+TITLE: %?\n#+DATE: %T\n#+TAGS: draft\n#+EID: %(org-id-uuid)\n\n")
           ("b" "Blog" plain (file (lambda ()
                                     (let* ((slug (read-string "slug: "))
-                                           (dir (concat "~/drive/org/public/unknownplace.org/blog"
+                                           (dir (concat "~/dev/src/github.com/typester/blog-entries/blog"
                                                         (format-time-string "/%Y/%m/%d"))))
                                       (require 'org-id)
                                       (make-directory dir t)

b28f65b3d4d5caabf40b5eb6156b9f290976fa5e
Daisuke Murase <typester@cpan.org>
2019-04-09 08:36:22 +0900

    specify plantuml.jar path at termux

--- conf/termux.el
+++ conf/termux.el
@@ -1,2 +1,5 @@
 ;; remap set-mark-command, because with my Custom SKK I can't use C-SPC.
 (define-key global-map (kbd "C-x SPC") 'set-mark-command)
+
+;; override pluntuml.jar path
+(setq plantuml-jar-path (expand-file-name "~/plantuml.jar"))

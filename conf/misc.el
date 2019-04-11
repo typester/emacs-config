509d69fbee523e0ae1b4606e4e152bffc3494e9f
Daisuke Murase <typester@cpan.org>
2019-04-11 10:53:46 +0900

    add lua-mode and pkgbuild-mode

--- conf/misc.el
+++ conf/misc.el
@@ -1,4 +1,8 @@
 ;; misc languages
-(el-get-bundle lua-mode)
+(el-get-bundle lua-mode
+  (setq lua-indent-level 4))
 (el-get-bundle emacs-fish)
 (el-get-bundle dockerfile-mode)
+(el-get-bundle pkgbuild-mode :type github :pkgname "juergenhoetzel/pkgbuild-mode" ; recipe bundled with el-get doesn't use autoload, so redefine it
+
+  (setq pkgbuild-update-sums-on-save nil))

36a870a40f6dd4a6d6e7fe94b8e89f4f7390e969
Daisuke Murase <typester@cpan.org>
2019-04-18 12:46:29 +0900

    add cmake-mode

--- conf/misc.el
+++ conf/misc.el
@@ -11,3 +11,4 @@
 (el-get-bundle yaml-mode)
 (el-get-bundle pkgbuild-mode :type github :pkgname "juergenhoetzel/pkgbuild-mode" ; recipe bundled with el-get doesn't use autoload, so redefine it
   (setq pkgbuild-update-sums-on-save nil))
+(el-get-bundle cmake-mode)

52db7d67194771db44eb944a168e6f060924eaf9
Daisuke Murase <typester@cpan.org>
2019-04-24 10:07:57 +0900

    use lsp-ui-peek as default ref find functions on lsp-mode

--- conf/lsp.el
+++ conf/lsp.el
@@ -1,7 +1,10 @@
 (el-get-bundle lsp-mode
   (setq lsp-prefer-flymake nil))
 (el-get-bundle lsp-ui
-  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  (with-eval-after-load-feature 'lsp-ui
+    (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+    (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)))
 (el-get-bundle yasnippet)
 (el-get-bundle company-lsp
   :type github

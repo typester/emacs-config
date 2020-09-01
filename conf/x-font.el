98f1ee5df0cbc2158ec61c808071fba8774a9905
Daisuke Murase <typester@cpan.org>
2020-09-01 12:49:40 -0700

    adjust font size on 4K monitor

--- conf/x-font.el
+++ conf/x-font.el
@@ -1,5 +1,9 @@
-(set-face-attribute 'default nil
-                    :family "Source Han Code JP" :height 110)
+(if (and (fboundp 'display-pixel-width)
+         (>= (display-pixel-width) 3840))
+    (set-face-attribute 'default nil
+                        :family "Source Han Code JP" :height 150)
+  (set-face-attribute 'default nil
+                      :family "Source Han Code JP" :height 110))
 
 ;; ↑だけだと日本語が違うfontになっちゃうことがある
 (dolist (code '(japanese-jisx0208 katakana-jisx0201))
